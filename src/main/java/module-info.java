4c74b8b849ea062071738b2240740971c42e3f9a
aaniksahaa <63545621+aaniksahaa@users.noreply.github.com>
2022-09-19 13:17:37 +0600

    Delete src/main/java directory

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module myjfx {
-    requires javafx.fxml;
-    requires javafx.controls;
-    requires javafx.graphics;
-    opens client to javafx.graphics, javafx.fxml;
-    opens controllers to javafx.fxml, javafx.graphics;
-}
